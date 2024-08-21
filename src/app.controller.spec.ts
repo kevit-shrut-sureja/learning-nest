6ca6fca600a5d6e122002497dc30eefbe7855e9c
kevit-shrut-sureja <shrut.sureja@kevit.io>
2024-08-21 10:47:08 +0530

    Health checkpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('server should be up and running!"', () => {
+      expect(appController.healthcheck()).toBe('Server is up and running...');
     });
   });
 });
